474f9511d46a957fdc8fbe8e8a4a87edb08cdc50
Rachel Kwon <rachel.kwon@gmail.com>
2025-02-07 19:03:17 -0500

    fix localhost error

--- public/js/links.min.b9618789012a25543ea8d884cb9d0b6e1cfaaf779bd0f0532b6bfb412979789c.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){const e="https://script.google.com/macros/s/AKfycbzjyQRiPiz2ejXyBD-68th_q-Mqe-oVQ4925VHfKsCCv7ROimh3fmNVbPujCF0F9nn0/exec?key=x5DA7Gxf8FnCFnDe";fetch(e).then(e=>{if(!e.ok)throw new Error(`HTTP error! Status: ${e.status}`);return e.json()}).then(e=>{const t=document.getElementById("linkroll");t.innerHTML="",e.forEach(e=>{const n=document.createElement("div");n.innerHTML=`
-          <li class="pinboard_link"><a href="${e.url}">${e.title}</a>
-          <span class="pinboard_desc">${e.description}</span>
-          </li>
-        `,t.appendChild(n)})}).catch(e=>{console.error("Error fetching data:",e),document.getElementById("dynamic-links").innerHTML="<p>Error loading links.</p>"})})
